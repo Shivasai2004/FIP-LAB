8073d8c3596f20e0658ca44ac383acd32725c24e
Shiva sai <94608449+Shivasai2004@users.noreply.github.com>
2024-05-11 09:33:13 +0530

    min max median and avg filter

--- remove_noise_avg_median.m
+++ remove_noise_avg_median.m
@@ -1,29 +1,33 @@
-original_image = imread('bridge.jpg');
-original_gray = rgb2gray(original_image);
-noisy_image = imnoise(original_gray, 'salt & pepper', 0.05);
+a = imread('cameraman.jpg');
+b = rgb2gray(a);
+noisy_img = imnoise(b, 'salt & pepper', 0.05);
 
-psnr_before = psnr(noisy_image, original_gray);
-mse_before = immse(noisy_image, original_gray);
+psnr_org = psnr(noisy_img, b);
+mse_org = immse(noisy_img, b);
 
-filtered_avg = imfilter(noisy_image, fspecial('average', [3 3]));
-filtered_med = medfilt2(noisy_image, [3 3]);
+filtered_avg = imfilter(noisy_img, fspecial('average', [3 3]));
+filtered_med = medfilt2(noisy_img, [3 3]);
 
-psnr_after_avg = psnr(filtered_avg, original_gray);
-mse_after_avg = immse(filtered_avg, original_gray);
+min_filtered = ordfilt2(noisy_img, 1, true(3));
+max_filtered = ordfilt2(noisy_img, 9, true(3));
 
-psnr_after_med = psnr(filtered_med, original_gray);
-mse_after_med = immse(filtered_med, original_gray);
+average_psnr = psnr(filtered_avg, b);
+average_mse = immse(filtered_avg, b);
+
+median_psnr = psnr(filtered_med, b);
+median_mse = immse(filtered_med, b);
+
+min_psnr = psnr(min_filtered, b);
+min_mse = immse(min_filtered, b);
+
+max_psnr = psnr(max_filtered, b);
+max_mse = immse(max_filtered, b);
 
 figure;
 
-subplot(2, 3, 1),imshow(original_gray),title('Original Image');
-subplot(2, 3, 2),imshow(noisy_image),title('Noisy Image (Salt & Pepper)');
-subplot(2, 3, 4),imshow(filtered_avg),title('After Average Filter');
-subplot(2, 3, 5),imshow(filtered_med),title('After Median Filter');
-
-annotation('textbox', [0.65, 0.75, 0.3, 0.15], 'String', ...
-    sprintf('PSNR Before: %.4f\nMSE Before: %.4f\n\nPSNR (Avg Filter): %.4f\nMSE (Avg Filter): %.4f\n\nPSNR (Med Filter): %.4f\nMSE (Med Filter): %.4f', ...
-    psnr_before, mse_before, psnr_after_avg, mse_after_avg, psnr_after_med, mse_after_med), 'FitBoxToText', 'on', 'BackgroundColor', 'w');
-
-% Adjust figure layout
-sgtitle('Noise Removal with Average and Median Filters');
+subplot(2, 3, 1),imshow(b),title('Original Image');
+subplot(2, 3, 2),imshow(noisy_img),title('Noisy Image (Salt & Pepper)');
+subplot(2, 3, 3),imshow(filtered_avg),title('After Average Filter');
+subplot(2, 3, 4),imshow(filtered_med),title('After Median Filter');
+subplot(2, 3, 5),imshow(min_filtered),title('After Minimum Filter');
+subplot(2, 3, 6),imshow(max_filtered),title('After Maximum Filter');
